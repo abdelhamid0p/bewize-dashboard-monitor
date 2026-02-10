3cd4598d50e517cca696693829fd88dac26c3fc6
Abdelhamid-p0 <abdelhamid.hachimi.alaoui@gmail.com>
2026-02-10 14:57:50 +0100

    feature/FE-02-2-1-login-page-static_auth

---
+++ src/features/auth/services/authService.ts
@@ -0,0 +1,18 @@
+import type { AppDispatch } from '@/app/store'
+import { loginStart, loginSuccess, loginFailure } from '../store/authSlice'
+
+export const login =
+  (email: string, password: string) =>
+  async (dispatch: AppDispatch) => {
+    dispatch(loginStart())
+
+    await new Promise((res) => setTimeout(res, 1000))
+
+    if (email === 'admin@test.com' && password === '123456') {
+      dispatch(loginSuccess({ email }))
+    } else if (email === 'admin@test.com' && password != '123456'){
+      dispatch(loginFailure('Mot de passe incorrect'))
+    }else {
+      dispatch(loginFailure('Email incorret'))
+    }
+  }
---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,5 @@
+ 
+export { useAppSelector } from "./useAppSelector";
+
+export { useAppDispatch } from "./useAppDispatch";
+
