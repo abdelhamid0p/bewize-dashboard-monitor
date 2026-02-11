ef8c61f3be1eca755726cdf30e9492125c1ef521
Abdelhamid-p0 <abdelhamid.hachimi.alaoui@gmail.com>
2026-02-11 18:45:12 +0100

    fix/F03-Sidebar : adding stash

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,10 @@
 import '@/design-system/colors.css'  // vos variables de couleurs
 import '@/design-system/typography.css' // vos variables de typo
 import '@/index.css'             // Tailwind
-import StaticDesignSystemShowcase from "@/design-system/design-system-showcase/staticShowcase.tsx";
-import DynamicDesignSystemShowcase from "@/design-system/design-system-showcase/interactiveShowcase.tsx";
+import StaticDesignSystemShowcase from "@/shared/design-system/design-system-showcase/staticShowcase";
+import DynamicDesignSystemShowcase from "@/shared/design-system/design-system-showcase/interactiveShowcase";
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 
 
 function App() {
@@ -16,6 +15,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<StaticDesignSystemShowcase />} />
                 <Route path="/dynamic_showcase" element={<DynamicDesignSystemShowcase />} />
+
+
             </Routes>
         </BrowserRouter>
 
