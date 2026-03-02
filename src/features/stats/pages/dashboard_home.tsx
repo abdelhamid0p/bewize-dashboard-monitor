1c3953bd83f92817041f2b073c5f516b322f32c3
Abdelhamid-p0 <abdelhamid.hachimi.alaoui@gmail.com>
2026-03-02 12:38:17 +0100

    fix padding dashboard

--- src/features/stats/pages/dashboard_home.tsx
+++ src/features/stats/pages/dashboard_home.tsx
@@ -7,19 +7,19 @@ export const DashboardPage = () => {
   const dashboard = useDashboard();
 
   return (
-    <div className="p-3 lg:p-4 xl:p-6">
+    <div className="p-3 lg:p-4 xl:p-6 space-y-3 lg:space-y-4 bg-[#FAFAFF] min-h-full">
       {/* Dashboard Navbar */}
       <DashboardNavbar userName="Mohammed" />
 
       {/* Number Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 lg:gap-4 xl:gap-6 mt-4 lg:mt-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 lg:gap-4 xl:gap-6">
         {dashboard.data?.stats.map((stat) => (
           <StatsNumberCardContainer key={stat.id} {...stat} />
         ))}
       </div>
 
       {/* Charts Cards - Grid 2x2 responsive */}
-      <div className="grid grid-cols-1 xl:grid-cols-2 gap-3 lg:gap-4 xl:gap-6 mt-4 lg:mt-6">
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-3 lg:gap-4 xl:gap-6">
         {dashboard.data?.charts.map((chart) => (
           <div
             key={chart.id}
