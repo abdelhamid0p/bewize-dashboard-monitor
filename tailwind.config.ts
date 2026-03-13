3aa6dc627729c86691d0cd9233ee9f46795f4530
Abdelhamid-p0 <abdelhamid.hachimi.alaoui@gmail.com>
2026-03-13 07:51:48 +0100

    Ajouter le commandes de deploiment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,3 +3,4 @@ module.exports = {
     content: ['./index.html', './src/**/*.{ts,tsx}'],
 
 };
+/* npm run build; npx gh-pages -d dist -r https://github.com/abdelhamid0p/bewize-dashboard-monitor.git*/
